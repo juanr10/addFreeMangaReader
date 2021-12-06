31feee065e4a3cdb494cec49b72d5dde4e70c0ab
juanr10 <argudojuan7@gmail.com>
2021-12-06 20:44:17 +0100

    add Navbar component

---
+++ components/Layout/Layout.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import NavBar from './NavBar';
+import Head from 'next/head';
+
+const Layout = (props) => {
+  return (
+    <>
+      <Head>
+        <title>Add Free Manga Reader</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <NavBar />
+      <main>{props.children}</main>
+    </>
+  );
+};
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,9 @@
+import Layout from "../components/layout/Layout";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen justify-center items-center">
-      <div className="max-w-md rounded overflow-hidden shadow-lg my-2">
-        <img
-          className="w-full"
-          src="https://tailwindcss.com/img/card-top.jpg"
-          alt="Sunset in the mountains"
-        />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Next + Tailwind ❤️</div>
-          <p className="text-grey-darker text-base">
-            Next and Tailwind CSS are a match made in heaven, check out this
-            article on how you can combine these two for your next app.
-          </p>
-        </div>
-      </div>
+    <div className="w-full min-h-screen">
+      <Layout></Layout>
     </div>
   );
 }
